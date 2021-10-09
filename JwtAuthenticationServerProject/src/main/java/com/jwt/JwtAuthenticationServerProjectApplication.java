7845e74406c3f57b6e19406b8bf17226e2983bd1
Bhola Kumar <bholakumar.7372@gmail.com>
2021-10-09 22:01:09 +0530

    database updated

--- JwtAuthenticationServerProject/src/main/java/com/jwt/JwtAuthenticationServerProjectApplication.java
+++ JwtAuthenticationServerProject/src/main/java/com/jwt/JwtAuthenticationServerProjectApplication.java
@@ -1,13 +1,18 @@
 package com.jwt;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.jwt.model.User;
+import com.jwt.repo.UserRepository;
 
 @SpringBootApplication
 public class JwtAuthenticationServerProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JwtAuthenticationServerProjectApplication.class, args);
+
 	}
 
 }
--- JwtAuthenticationServerProject/src/main/java/com/jwt/services/CustomUserDetailsService.java
+++ JwtAuthenticationServerProject/src/main/java/com/jwt/services/CustomUserDetailsService.java
@@ -2,22 +2,39 @@ package com.jwt.services;
 
 import java.util.ArrayList;
 
-import org.springframework.security.core.userdetails.User;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.jwt.model.CustomUserDetails;
+import com.jwt.model.User;
+import com.jwt.repo.UserRepository;
+
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
+	@Autowired
+	private UserRepository userRepository;
+
 	@Override
 	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-		if (userName.equals("rakesh")) {
-			return new User("rakesh", "rakesh123", new ArrayList<>());
-		} else {
+
+		User user = this.userRepository.findByUsername(userName);
+
+		if (user == null) {
 			throw new UsernameNotFoundException("user not found!!");
+		} else {
+			return new CustomUserDetails(user);
 		}
+
+		/*
+		 * if (userName.equals("rakesh")) { return new User("rakesh", "rakesh123", new
+		 * ArrayList<>()); } else { throw new
+		 * UsernameNotFoundException("user not found!!"); }
+		 */
 
 	}
 
